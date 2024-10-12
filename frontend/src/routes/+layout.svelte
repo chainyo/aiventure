1ece79429f2f937f081bd18f4b943592419bc4e9
chainyo <t.chaigneau.tc@gmail.com>
2024-10-12 16:17:52 +0200

    add sonner and modewatcher

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
+	import { ModeWatcher } from "mode-watcher";
+	import { Toaster } from "$lib/components/ui/sonner";
 	import '../app.css';
 </script>
 
+<ModeWatcher />
+<Toaster />
 <slot></slot>
