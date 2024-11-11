0b9fbfb2e8a4618128f83ff017cffc44320aa1c9
chainyo <t.chaigneau.tc@gmail.com>
2024-11-11 11:07:02 +0100

    feat: setup frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	import { ModeWatcher } from "mode-watcher";
+	import { Toaster } from "$lib/components/ui/sonner/index.js";
+	let { children } = $props();
+</script>
+
+<ModeWatcher />
+<Toaster richColors expand={true} />
+{@render children()}
