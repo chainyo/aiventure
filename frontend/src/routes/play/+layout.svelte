d819561f9dab6607f75a92f2e366e7de8fc8a6de
chainyo <t.chaigneau.tc@gmail.com>
2024-11-24 11:40:05 +0100

    feat: add bottom ctrl buttons

--- frontend/src/routes/play/+layout.svelte
+++ frontend/src/routes/play/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import { onMount, onDestroy } from 'svelte';
     import { goto } from "$app/navigation";
-    import { ChevronDown, LoaderCircle } from "lucide-svelte";
+    import { ChevronDown, LoaderCircle, Moon, Sun } from "lucide-svelte";
+    import { toggleMode } from "mode-watcher";
 
     import { GameWebSocketClient } from "$lib/websocket";
     import { GameActions, type GameMessageResponse } from "$lib/types/websocket";
@@ -153,7 +154,7 @@
         </Sidebar.Header>
         <Sidebar.Content>
             <Sidebar.Group />
-                <Button variant="outline" onclick={() => { userStore.clearUser(); goto('/login'); }}>Logout</Button>
+
             <Sidebar.Group />
         </Sidebar.Content>
         <Sidebar.Footer class="pb-8">
@@ -186,6 +187,23 @@
                         </Sidebar.MenuItem>
                     </div>
                 {/if}
+            </Sidebar.Menu>
+            <Sidebar.Menu class="flex flex-row justify-center gap-2 mt-8">
+                <Button onclick={toggleMode} variant="outline" size="icon">
+                    <Sun
+                    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+                    />
+                    <Moon
+                    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+                    />
+                    <span class="sr-only">Toggle theme</span>
+                </Button>
+                <Button onclick={() => { window.open('https://github.com/chainyo/aiventure', '_blank'); }} variant="outline" size="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+                </Button>
+                <Button onclick={() => { userStore.clearUser(); goto('/login'); }} variant="outline" size="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" x2="9" y1="12" y2="12"/></svg>
+                </Button>
             </Sidebar.Menu>
         </Sidebar.Footer>
     </Sidebar.Root>
